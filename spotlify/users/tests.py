136c6c8c27c22a3b52dec48f6e8b9c086a615a38
Josh Dias <dias.joshua7@yahoo.com>
2023-06-15 16:33:50 -0700

    Update import statements

--- spotlify/users/tests.py
+++ spotlify/users/tests.py
@@ -1,9 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from rest_framework.test import APIClient
-from .models import User
-import pytest
 import json
 
 
